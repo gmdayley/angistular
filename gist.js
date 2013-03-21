670e1eb0e64fbd438a4430c0b7d0238000903647
Gabriel Dayley <gabriel.dayley@gmail.com>
2013-03-20 20:18:24 -0600

    step3 complete

--- gist.js
+++ gist.js
@@ -16,10 +16,17 @@ angular.module('app').controller('GistController', function ($scope, $http, $log
     // Create a ng model object to hold our gists
     $scope.gists = {};
 
-    var Gist = $resource('https://api.github.com/gists/:id', {id: '@id', access_token: accessToken});
+    var Gist = $resource('https://api.github.com/gists/:id/:option', {id: '@id', access_token: accessToken}, {
+        public: {method: 'GET', params: {id: 'public'}, isArray: true},
+        starred: {method: 'GET', params: {id: 'starred'}, isArray: true}
+    });
 
     // Query list of users gist
     $scope.gists.myGists = Gist.query();
+
+    // Query list of public gists
+    $scope.gists.publicGists = Gist.public();
+
 
     // Create a new gist
     $scope.new = function () {
