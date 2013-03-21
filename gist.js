71bce88275adc435ed23f4b6d9ce41f5c49adb2d
Gabriel Dayley <gabriel.dayley@gmail.com>
2013-03-20 20:05:52 -0600

    step2 complete

--- gist.js
+++ gist.js
@@ -16,8 +16,31 @@ angular.module('app').controller('GistController', function ($scope, $http, $log
     // Create a ng model object to hold our gists
     $scope.gists = {};
 
-    var Gist = $resource('https://api.github.com/gists', {access_token: accessToken});
+    var Gist = $resource('https://api.github.com/gists/:id', {id: '@id', access_token: accessToken});
 
     // Query list of users gist
     $scope.gists.myGists = Gist.query();
+
+    // Create a new gist
+    $scope.new = function () {
+        var g = new Gist({
+            "description": "A new gist created by AnGistular",
+            "public": false,
+            "files": {
+                "file1.txt": {
+                    "content": "file contents"
+                }
+            }
+        });
+
+        g.$save();
+        $scope.gists.myGists.push(g);
+    };
+
+    // Remove a gist
+    $scope.remove = function(gist) {
+        gist.$remove();
+        var index = $scope.gists.myGists.indexOf(gist);
+        $scope.gists.myGists.splice(index, 1);
+    }
 });
